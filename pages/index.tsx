be62d97fb3fc6928567c7cadfbdce2cd82780329
ivancraze <ivancraze@yandex.ru>
2021-07-05 11:07:03 +0400

    add paragraph and tag components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Htag } from '../components';
+import { Button, Htag, P, Tag } from '../components';
 
 export default function Home(): JSX.Element {
 	return (
@@ -7,6 +7,13 @@ export default function Home(): JSX.Element {
 			<Htag tag='h1'>Текст</Htag>
 			<Button appearance='primary' arrow='right'>Кнопка</Button>
 			<Button appearance='ghost' arrow='down'>Кнопка</Button>
+			<P size='l'>Большой</P>
+			<P>Средний</P>
+			<P size='s'>Малый</P>
+			<Tag size='s'>Ghost</Tag>
+			<Tag size='m' color='red'>Red</Tag>
+			<Tag size='s' color='green'>Green</Tag>
+			<Tag size='m' color='primary'>primary</Tag>
 		</>
 	);
 }
