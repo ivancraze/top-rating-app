44b06cbdbdc07407b5a8c2f5262707c43f8f0b39
ivancraze <ivancraze@yandex.ru>
2021-07-15 18:09:00 +0400

    add context

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,9 +22,6 @@ function Home({ menu }: HomeProps): JSX.Element {
 			<Tag size='m' color='primary'>primary</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
 
-			<ul>
-				{menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
-			</ul>
 		</>
 	);
 }
